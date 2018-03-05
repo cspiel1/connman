e6aec827d3fd6e819734cbf64ba8bbbcb7dfc0b1
Christian Spielberger <christian.spielberger@gmail.com>
2018-03-05 11:28:41 +0100

    acd: add callback registration
    Adds function acdhost_register_event for registration of higher level address
    conflict callback functions.

--- include/acd.h
+++ include/acd.h
@@ -40,6 +40,18 @@ void acdhost_stop(acd_host *acd);
 
 typedef void (*ACDHostEventFunc) (acd_host *acd, gpointer user_data);
 
+typedef enum {
+	ACDHOST_EVENT_IPV4_AVAILABLE,
+	ACDHOST_EVENT_IPV4_LOST,
+	ACDHOST_EVENT_IPV4_CONFLICT,
+	ACDHOST_EVENT_IPV4_MAXCONFLICT,
+} ACDHostEvent;
+
+void acdhost_register_event(acd_host *acd,
+			    ACDHostEvent event,
+			    ACDHostEventFunc func,
+			    gpointer user_data);
+
 #ifdef __cplusplus
 }
 #endif
