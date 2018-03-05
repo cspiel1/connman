f7e084b7d84fc2b7c36e35ca379ab500fd3cf8af
Christian Spielberger <christian.spielberger@gmail.com>
2018-03-05 10:56:59 +0100

    acd: add send_announce_packet

--- src/acd.c
+++ src/acd.c
@@ -55,6 +55,9 @@ static gboolean acd_listener_event(GIOChannel *channel, GIOCondition condition,
 static int acd_recv_arp_packet(ACDHost *acd);
 static gboolean send_probe_packet(gpointer acd_data);
 static gboolean acd_probe_timeout(gpointer acd_data);
+static gboolean send_announce_packet(gpointer acd_data);
+static gboolean acd_announce_timeout(gpointer acd_data);
+static gboolean acd_defend_timeout(gpointer acd_data);
 
 static void debug(ACDHost *acd, const char *format, ...)
 {
@@ -229,3 +232,42 @@ static gboolean acd_probe_timeout(gpointer acd_data)
 	return FALSE;
 }
 
+static void remove_timeouts(ACDHost *adc)
+{
+
+	if (adc->timeout > 0)
+		g_source_remove(adc->timeout);
+
+	adc->timeout = 0;
+}
+
+static gboolean send_announce_packet(gpointer acd_data)
+{
+	ACDHost *acd = acd_data;
+
+	debug(acd, "sending ACD announce request");
+
+	send_arp_packet(acd->mac_address,
+				acd->requested_ip,
+				acd->requested_ip,
+				acd->ifindex);
+
+	remove_timeouts(acd);
+
+	if (acd->state == ACD_DEFEND) {
+		acd->timeout =
+			g_timeout_add_seconds_full(G_PRIORITY_HIGH,
+						DEFEND_INTERVAL,
+						acd_defend_timeout,
+						acd,
+						NULL);
+		return TRUE;
+	} else
+		acd->timeout =
+			g_timeout_add_seconds_full(G_PRIORITY_HIGH,
+						ANNOUNCE_INTERVAL,
+						acd_announce_timeout,
+						acd,
+						NULL);
+	return TRUE;
+}
