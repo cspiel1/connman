a54a1a167456f9e1a37932dbad44bb11a9d56c0d
Christian Spielberger <christian.spielberger@gmail.com>
2018-04-04 15:25:47 +0200

    Add Address Conflict Detection support (RFC 5227)
    Add first acd.h, acd.c, a configuration ACD on/off switch and a first
    version of start_acd in network.c which does nothing for the moment.

---
+++ include/acd.h
@@ -0,0 +1,38 @@
+/*
+ *
+ *  Connection Manager
+ *
+ *  Address Conflict Detection (RFC 5227)
+ *
+ *  based on DHCP client library with GLib integration,
+ *      Copyright (C) 2009-2014  Intel Corporation. All rights reserved.
+ *
+ *  Copyright (C) 2018  Commend International. All rights reserved.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License version 2 as
+ *  published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ */
+
+#ifndef __CONNMAN_ACD_H
+#define __CONNMAN_ACD_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+struct _acd_host;
+
+typedef struct _acd_host acd_host;
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __CONNMAN_ACD_H */
